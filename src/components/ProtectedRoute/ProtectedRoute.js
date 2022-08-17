feae99a2392eba0e24bfa6235b45cef972c96e9c
Alexander Grigorovich <crit1k8912@gmail.com>
2022-08-17 10:27:40 +0300

    fix protected route

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
 
-const ProtectedRoute = ({ component: Component, ...props }) => {
+const ProtectedRoute = ({ loggedIn, children}) => {
   return (
     <Route>
       {
-      () => props.loggedIn ? <Component {...props} /> : <Redirect to="/signin" />
+      () => loggedIn ? {children} : <Redirect to="/signin" />
       }
     </Route>
   )
 }
 
 export default ProtectedRoute;
+
