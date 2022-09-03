2fa9bfdccf4e0378d0cae49284dc38173572d6ec
Alexander Grigorovich <crit1k8912@gmail.com>
2022-09-03 13:46:36 +0300

    change api link

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-export const BASE_URL = "//localhost:3001";
-// export const BASE_URL = `${window.location.protocol}//api.algrigorovich.nomoredomains.xyz`;
+// export const BASE_URL = "//localhost:3001";
+export const BASE_URL = `${window.location.protocol}//api.algrigorovich.nomoredomains.xyz`;
 export const MOVIES_URL = "https://api.nomoreparties.co/beatfilm-movies";
 export const MAX_SHORTMOVIES_DURATION = 40;
 export const MAX_MOBILE_WIDTH = 768;
